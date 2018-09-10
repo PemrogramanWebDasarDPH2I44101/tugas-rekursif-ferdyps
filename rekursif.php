f3b3f9b2d03933e4bf0c43c23e874e21a01dd849
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-09-10 18:26:17 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,37 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+  $i = 0;
+	rekursif(5); 
+	function rekursif($a){
+		global $i;
+		newrekursif($i,$a);
+		echo "<br>";
+		if (--$a > $i) {
+			rekursif($a);
+		}
+	}
+	function newrekursif($j,$a){
+		global $i;
+		if ($j > $i - $a) {
+			echo "*";
+			newrekursif($j - 1,$a);
+		}
+	}
+	echo "<br>====================================================================<br>";
+	$column = 5;
+	rek(0,1);
+	function rek($q,$c){
+		global $column;
+		newrek($column,$q,$c);
+		echo "<br>";
+		if (++$q < $column) {
+			rek($q,$c);
+		}
+	}
+	function newrek($p,$q,$r){
+		global $column;
+		if ($p > $column - $q - 1) {
+			echo $r;
+			newrek($p - 1,$q,$r + 1);
+		}
+	}
 ?>
